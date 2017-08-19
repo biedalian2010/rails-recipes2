6b65d1cffbfeeab58ea7516632f6c779e43bdf7a
李阳 <admin@admindeMacBook-Air.local>
2017-08-19 12:19:17 +0800

    9-3 嵌套(Nested)表单 UI

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  accepts_nested_attributes_for :profile
 
   has_many :memberships
   has_many :groups, :through => :memberships
